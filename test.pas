38452b70a2589a6eecd5f9cf60c44ea7f955c34a
Tera-bytes <49597727+Tera-bytes@users.noreply.github.com>
2019-04-13 23:41:09 +0300

    Create test.pas
    I just started the program

---
+++ test.pas
@@ -0,0 +1,14 @@
+program prog3(input,output);
+var a,b:integer;
+    c:real;
+begin
+write('a='); read(a);
+write('b='); read(b);
+write('c='); read(c);
+write('a=',a);
+write('b=',b);
+write('a',a:5);
+writeln('c=',c)
+writeln('c',c:5:1);
+end.
+
